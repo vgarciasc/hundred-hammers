343a52a42e718400660b2a4ccbab326011017c59
Vinícius Garcia <13024544+vgarciasc@users.noreply.github.com>
2023-09-15 11:00:00 -0300

    adding 'seed_strategy' as parameter to hh models

--- hundred_hammers/regressor.py
+++ hundred_hammers/regressor.py
@@ -9,11 +9,11 @@ class HundredHammersRegressor(HundredHammersBase):
     """
 
     def __init__(self, models=None, metrics=None, eval_metric=None, test_size=0.2,
-                 n_folds=5, n_folds_tune=5, n_seeds=10):
+                 n_folds=5, n_folds_tune=5, n_seeds=10, seed_strategy='sequential'):
         if models is None:
             models = deepcopy(DEFAULT_REGRESSION_MODELS)
 
         if metrics is None:
             metrics = copy(DEFAULT_REGRESSION_METRICS)
 
-        super().__init__(models, metrics, eval_metric, test_size, n_folds, n_folds_tune, n_seeds)
+        super().__init__(models, metrics, eval_metric, test_size, n_folds, n_folds_tune, n_seeds, seed_strategy)
