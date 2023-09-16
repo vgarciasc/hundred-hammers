97ce89ac548b7ff64d054445e1019556f5aa61fb
Vinícius Garcia <13024544+vgarciasc@users.noreply.github.com>
2023-09-15 21:35:04 -0300

    adding optional progress bar powered by rich

--- hundred_hammers/regressor.py
+++ hundred_hammers/regressor.py
@@ -16,15 +16,17 @@ class HundredHammersRegressor(HundredHammersBase):
     :param n_folds: Number of Cross Validation folds (default 5)
     :param n_folds_tune: Number of Cross Validation folds in grid search (default 5)
     :param n_evals: Number of times to repeat the training of the models (default 10)
+    :param show_progress_bar: Show progress bar in the evaluation (default False)
     :param seed_strategy: Strategy used to generate the seeds for the different evaluations ('sequential' or 'random')
     """
 
     def __init__(self, models=None, metrics=None, eval_metric=None, test_size=0.2,
-                 n_folds=5, n_folds_tune=5, n_evals=10, seed_strategy='sequential'):
+                 n_folds=5, n_folds_tune=5, n_evals=10, show_progress_bar=False, seed_strategy='sequential'):
         if models is None:
             models = deepcopy(DEFAULT_REGRESSION_MODELS)
 
         if metrics is None:
             metrics = copy(DEFAULT_REGRESSION_METRICS)
 
-        super().__init__(models, metrics, eval_metric, test_size, n_folds, n_folds_tune, n_evals, seed_strategy)
+        super().__init__(models, metrics, eval_metric, test_size, n_folds, n_folds_tune,
+                         n_evals, show_progress_bar, seed_strategy)
