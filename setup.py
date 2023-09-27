16389dbf030ae1d28df07621cc8e7036c19c24a0
vgarcia <13024544+vgarciasc@users.noreply.github.com>
2023-09-27 15:58:18 -0300

    updating to 1.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hundred_hammers',
-      version='1.0.4',
+      version='1.0.5',
       description='Batch execution of ML models to a dataset',
       url='http://github.com/vgarciasc/hundred_hammers',
       author='Vinicius Garcia',
@@ -10,7 +10,7 @@ setup(name='hundred_hammers',
       packages=['hundred_hammers'],
       install_requires=[
           "seaborn", "matplotlib", "adjustText",
-          "scikit-learn", "scikit-elm", "xgboost" 
+          "scikit-learn", "scikit-elm", "xgboost>=1.7.6",
           "pandas", "tqdm", "dask", "dask[distributed]",
           "schema", "pytest"],
       zip_safe=False)
