6ec1707ce02896ea15d3cf868aa8ddefaab8c45d
vgarcia <13024544+vgarciasc@users.noreply.github.com>
2023-09-27 15:37:27 -0300

    adding install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
 setup(name='hundred_hammers',
-      version='0.1.1',
+      version='1.0.4',
       description='Batch execution of ML models to a dataset',
       url='http://github.com/vgarciasc/hundred_hammers',
       author='Vinicius Garcia',
       author_email='vinizinho@vinizinho.net',
       license='MIT',
       packages=['hundred_hammers'],
+      install_requires=[
+          "seaborn", "matplotlib", "adjustText",
+          "scikit-learn", "scikit-elm", "xgboost" 
+          "pandas", "tqdm", "dask", "dask[distributed]",
+          "schema", "pytest"],
       zip_safe=False)
